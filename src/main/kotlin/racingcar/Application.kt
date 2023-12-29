d1659a10b95726b1a6228126d09d32eb4b193ba2
hyun <sarahs19991115@gmail.com>
2023-12-29 14:00:42 +0900

    feat($setCars): 입력받은 자동차 이름만큼의 자동차 객체 생성 및 초기 위치 설정
    refactor($GameManager, $Application): 입력받고 자동차 셋팅하는 부분의 함수 이름 변경
    
    Before:
    startGame
    
    After:
    setGame

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,5 +3,5 @@ package racingcar
 import racingcar.domain.GameManager as GAME
 
 fun main() {
-    GAME.startGame()
+    GAME.setGame()
 }
--- src/main/kotlin/racingcar/domain/GameManager.kt
+++ src/main/kotlin/racingcar/domain/GameManager.kt
@@ -4,9 +4,17 @@ import racingcar.ui.InputView as INPUT
 
 class GameManager {
     companion object {
-        fun startGame() {
+        fun setGame() {
             val carNames = INPUT.readCarNames()
             val tryCounts = INPUT.readTryCounts()
+
+            val cars = setCars(carNames)
+        }
+
+        private fun setCars(carNames: List<String>): List<Car> {
+            return carNames.map { Car(it, 0) }
         }
     }
 }
+
+data class Car(val name: String, var position: Int)
