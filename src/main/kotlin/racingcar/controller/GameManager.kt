294f07e5984c66f5999d4c74008e1d09151badb6
hyun <sarahs19991115@gmail.com>
2024-01-12 23:30:19 +0900

    refactor($review by bandal): 반달님 2차 코드 리뷰 기반으로 코드 전체 리팩토링4
    리뷰 기반 수정 내용:
    - GameManager 싱글톤으로 변경 (다른 방법으로는 GameManager를 그대로 클래스로 두고 Application에서 GameManager 클래스를 인스턴스화시켜 함수를 호출할 수 있으나, 한 번만 실행되므로 Object로 싱글톤화 하는 것이 더 적절하다고 판단함)

--- src/main/kotlin/racingcar/controller/GameManager.kt
+++ src/main/kotlin/racingcar/controller/GameManager.kt
@@ -9,71 +9,69 @@ import kotlin.random.Random
 
 private const val RANDOM_RANGE = 10
 
-class GameManager {
-    companion object {
-        private val raceList = Race()
-        private val tryCounts = TryCounts()
-        private val outputView = OutputView()
+object GameManager {
+    private val raceList = Race()
+    private val tryCounts = TryCounts()
+    private val outputView = OutputView()
 
-        fun setGame() {
-            val carNamesInput = InputView.readCarNames()
-            carNamesInput?.let { setCars(it) }
+    fun setGame() {
+        val carNamesInput = InputView.readCarNames()
+        carNamesInput?.let { setCars(it) }
 
-            val tryCountsInput = InputView.readTryCounts()
-            tryCounts.tryCounts = tryCountsInput
+        val tryCountsInput = InputView.readTryCounts()
+        tryCounts.tryCounts = tryCountsInput
+    }
+
+    private fun setCars(carNames: List<String>) {
+        for (carName in carNames) {
+            val car = Car(carName, 0)
+            raceList.addCarList(car)
         }
+    }
 
-        private fun setCars(carNames: List<String>) {
-            for(carName in carNames) {
-                val car = Car(carName, 0)
-                raceList.addCarList(car)
+
+    fun startGame() {
+        outputView.startPrintEachResult()
+
+        val tryCounts = tryCounts.tryCounts
+        repeat(tryCounts) {
+            playOneRound()
+            printPlayResult()
+        }
+    }
+
+    fun playOneRound() {
+        val carList = raceList.cars
+        for (car in carList) {
+            val rand = Random.nextInt(RANDOM_RANGE)
+            car.updatePosition = rand
+        }
+    }
+
+    private fun printPlayResult() {
+        val carList = raceList.cars
+        for (car in carList) {
+            val carName = car.name
+            val position = car.position
+            outputView.printCarResult(carName, position)
+            println()
+        }
+    }
+
+
+    fun getWinner() {
+        val carList = raceList.cars
+        val maxPosition = carList.maxBy { it.position }.position
+
+        for (car in carList) {
+            val carName = car.name
+            val position = car.position
+
+            if (position == maxPosition) {
+                raceList.addWinnerList(carName)
             }
         }
 
-
-        fun startGame() {
-            outputView.startPrintEachResult()
-
-            val tryCounts = tryCounts.tryCounts
-            repeat(tryCounts) {
-                playOneRound()
-                printPlayResult()
-            }
-        }
-
-        fun playOneRound() {
-            val carList = raceList.cars
-            for(car in carList) {
-                val rand = Random.nextInt(RANDOM_RANGE)
-                car.updatePosition = rand
-            }
-        }
-
-        private fun printPlayResult() {
-            val carList = raceList.cars
-            for(car in carList) {
-                val carName = car.name
-                val position = car.position
-                outputView.printCarResult(carName, position)
-                println()
-            }
-        }
-
-
-        fun getWinner() {
-            val carList = raceList.cars
-            val maxPosition = carList.maxBy { it.position }.position
-
-            for(car in carList) {
-                val carName = car.name
-                val position = car.position
-
-                if(position == maxPosition) {
-                    raceList.addWinnerList(carName)
-                }
-            }
-
-            outputView.printWinners()
-        }
+        outputView.printWinners()
     }
 }
